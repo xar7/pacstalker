474114f074d3b94bec94d82cb71a8ef158bb7c05
Mathieu Nativel <mathieu.nativel@lse.epita.fr>
2019-03-10 19:38:22 +0100

    script: add arg checking and size printing

--- src/test.py
+++ src/test.py
@@ -1,19 +1,31 @@
+from sys import argv
 from scapy.all import *
 
-load_layer("tls")
-packets = rdpcap("test_pacman.pcap")
+if (len(argv) == 1):
+    print("Non interactiv mode of pacstalker is to be used with an input file.")
+    exit(1)
 
-counter = 0
+if (len(argv) > 2):
+    print("Pacstalker can only deal with one input file at a time.")
+    exit(1)
+
+load_layer("tls")
+packets = rdpcap(argv[1])
+pkg_session = []
+total_size = 0
+
 session_counter = 0
+
 for pkt in packets:
-    if TCP in pkt:
-        if TLS in pkt:
-            #pkt.show()
-            print(f"packet len = {pkt.len} && tls layer len = {pkt[TLS].len}")
-            print(pkt[TLS].type)
-            if pkt[TLS].type == 20: # 20 means ChangeCipherSpec
-                session_counter += 1
-                for tls_msg in pkt[TLS].msg:
-                    print(type(tls_msg))
-        counter += 1
-print(f"{session_counter} tls sessions were catched!")
+    total_size += pkt.len
+    if TLS in pkt:
+        if pkt[TLS].type == 20: # 20 means ChangeCipherSpec
+            pkg_session.append(0)
+        elif pkt[TLS].type == 23: # ApplicationData
+            pkg_session[-1] += pkt.len
+print(f"{len(pkg_session)} tls sessions were catched!")
+
+for i in range(len(pkg_session)):
+    print(f"Session number {i} transmits {pkg_session[i]} bytes of data.")
+
+print(f"{total_size} bytes were transferred during the record.")
